3a1c2aa66bfe840c449d3ae44a844f1fabd0a9c2
Anthony Cole <anthony.cole83@yahoo.com>
2023-02-09 17:25:18 -0600

    tried making a function for stretch goal 9.

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -98,11 +98,15 @@ console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll(array) {
   let sum = 0
+    for(sum; sum < array.length; ++sum){
+      sum += sumAll[sum];
+    }
+    
   // TODO: loop to add items
   return sum;
-}
+} console.log('Number 9 answer is:', sumAll([]));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
